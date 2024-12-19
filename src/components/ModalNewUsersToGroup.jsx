1122cb208dee6240b59dfb4481bb0725e4b2d5bd
Marst313 <dlegoinyoman1@gmail.com>
2024-12-19 10:51:06 +0700

    update list modal mahasiswa

--- src/components/ModalNewUsersToGroup.jsx
+++ src/components/ModalNewUsersToGroup.jsx
@@ -4,10 +4,10 @@ import { useParams } from 'react-router-dom';
 
 import useGroupStore from '../features/groupStore';
 import { FooterModal, HeaderModal } from './';
+import { getAllUser } from '../services/userServices';
 
 function ModalUsersGroup() {
-  const { modalGroupUsers, isLoading, listMahasiswaCanAdd, setModalGroupUsers, getAllGroup, connectUserToGroup } = useGroupStore((state) => state);
-
+  const { modalGroupUsers, isLoading, listMahasiswaCanAdd, setModalGroupUsers, getAllGroup, connectUserToGroup, setListMahasiswaCanAdd } = useGroupStore((state) => state);
   const { idActivity, idGroup } = useParams();
 
   const [selectedUsers, setSelectedUsers] = useState([]);
@@ -38,7 +38,11 @@ function ModalUsersGroup() {
     e.preventDefault();
 
     await connectUserToGroup({ idGroup, idUser: selectedUsers });
+
     await getAllGroup(idActivity);
+
+    const { data } = await getAllUser(idActivity, idGroup);
+    setListMahasiswaCanAdd(data);
 
     setSelectedUsers([]);
   };
@@ -138,31 +142,39 @@ function BodyModal({ handleAddUserGroup, allUser, isLoading, setModalGroupUsers,
             </tr>
           </thead>
           <tbody>
-            {allUser?.map((user, index) => {
-              return (
-                <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <td className="w-4 p-4">
-                    <div className="flex items-center">
-                      <input
-                        id={`checkbox-table-search-${index}`}
-                        type="checkbox"
-                        onChange={() => handleCheckboxChange(user.id)}
-                        checked={selectedUsers.includes(user.id)}
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                      />
-                      <label htmlFor="checkbox-table-search-1" className="sr-only">
-                        checkbox
-                      </label>
-                    </div>
-                  </td>
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {user.nim}
-                  </th>
-                  <td className="px-6 py-4">{user.name}</td>
-                  <td className="px-6 py-4">{user.email}</td>
-                </tr>
-              );
-            })}
+            {allUser.length !== 0 ? (
+              allUser?.map((user, index) => {
+                return (
+                  <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id={`checkbox-table-search-${index}`}
+                          type="checkbox"
+                          onChange={() => handleCheckboxChange(user.id)}
+                          checked={selectedUsers.includes(user.id)}
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label htmlFor="checkbox-table-search-1" className="sr-only">
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {user.nim}
+                    </th>
+                    <td className="px-6 py-4">{user.name}</td>
+                    <td className="px-6 py-4">{user.email}</td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="4" className="text-center py-4">
+                  Tidak ada user yang bisa dimasukkan ke dalam grup.
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
 
--- src/pages/Admin/SingleAgendaDetail.jsx
+++ src/pages/Admin/SingleAgendaDetail.jsx
@@ -1,6 +1,6 @@
-import { IoArrowBack } from 'react-icons/io5';
+import { IoArrowBack, IoArrowForward } from 'react-icons/io5';
 import { Link, useParams, useSearchParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useAgendaStore from '../../features/agendaStore';
 import { LoadingSkeletonTable } from '../../components';
 
@@ -26,7 +26,6 @@ function SingleAgendaDetail() {
   return (
     <div>
       <HeaderSectionBack />
-
       <ContentSection agenda={singleAgenda} />
     </div>
   );
@@ -42,10 +41,32 @@ function HeaderSectionBack() {
     </div>
   );
 }
+
 function ContentSection({ agenda }) {
-  const gps = agenda?.gps; // Pastikan gps ada
+  const gps = agenda?.gps;
   const latitude = gps ? gps.split(',')[0] : '';
   const longitude = gps ? gps.split(',')[1] : '';
+
+  const [previewImage, setPreviewImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isImageLoading, setIsImageLoading] = useState(false);
+
+  // Array gambar
+  const images = [agenda?.gambar1 || agenda?.gambar1_b64, agenda?.gambar2 || agenda?.gambar2_b64].filter(Boolean);
+
+  const closePreview = () => {
+    setPreviewImage(null);
+  };
+
+  const handleNext = () => {
+    setIsImageLoading(true);
+    setCurrentIndex((prev) => (prev === 0 ? 1 : 0));
+  };
+
+  const handlePrev = () => {
+    setIsImageLoading(true);
+    setCurrentIndex((prev) => (prev === 1 ? 0 : 1));
+  };
 
   return (
     <>
@@ -55,15 +76,29 @@ function ContentSection({ agenda }) {
           Detail Agenda : <span className="font-semibold text-xl text-center capitalize text-slate-800">{agenda?.detail || 'Detail tidak tersedia'}</span>
         </h1>
       </div>
-      <div className="mt-5 flex flex-col gap-5 lg:flex-row items-start text-start justify-start ">
+      <div className="mt-5 flex flex-col gap-5 lg:flex-row items-start text-start justify-start  ">
         {/* Gambar Section */}
-        <div>
+        <div className="flex  gap-3">
           <h2 className="text-lg font-semibold">Gambar :</h2>
-          <img src={agenda?.gambar1 || agenda?.gambar1_b64 || 'default_image.jpg'} loading="lazy" alt="Gambar agenda" className="max-h-96 " />
+          <div className="flex gap-5">
+            {images.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                loading="lazy"
+                alt={`Gambar ${index + 1}`}
+                className="max-h-96 cursor-pointer hover:opacity-70"
+                onClick={() => {
+                  setPreviewImage(img);
+                  setCurrentIndex(index);
+                }}
+              />
+            ))}
+          </div>
         </div>
 
         {/* Map Section */}
-        <div>
+        <div className="flex gap-5 mb-20">
           <h2 className="text-lg font-semibold">Lokasi :</h2>
           {gps ? (
             <iframe className="h-96 w-96" title="Google Maps" src={`https://www.google.com/maps?q=${latitude},${longitude}&hl=id&z=15&output=embed`} allowFullScreen="" loading="lazy"></iframe>
@@ -72,6 +107,55 @@ function ContentSection({ agenda }) {
           )}
         </div>
       </div>
+
+      {/* Modal Preview */}
+      {previewImage && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50" onClick={closePreview}>
+          <button
+            className="absolute top-1/2 left-20 bg-white/30 rounded-full hover:scale-105 text-primary text-3xl font-bold"
+            onClick={(e) => {
+              e.stopPropagation();
+              handlePrev();
+            }}
+          >
+            <IoArrowBack />
+          </button>
+
+          {/* Tampilkan loader jika gambar belum selesai dimuat */}
+          {isImageLoading && (
+            <div className="absolute flex items-center justify-center">
+              <span className="text-white text-xl animate-pulse">Loading...</span>
+            </div>
+          )}
+
+          <img
+            src={images[currentIndex]}
+            alt="Preview"
+            className={`max-w-full max-h-[90%] mx-4 mb-4 ${isImageLoading ? 'opacity-0' : 'opacity-100'} transition-opacity duration-300`}
+            onLoad={() => setIsImageLoading(false)} // Gambar selesai dimuat
+            onClick={(e) => e.stopPropagation()} // Mencegah close saat klik gambar
+          />
+
+          <button
+            className="absolute top-5 right-5 hover:scale-105 text-red-500 text-3xl font-bold"
+            onClick={(e) => {
+              e.stopPropagation();
+              closePreview();
+            }}
+          >
+            &times;
+          </button>
+          <button
+            className="absolute top-1/2 right-20 bg-white/30 rounded-full hover:scale-105 text-primary text-3xl font-bold"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleNext();
+            }}
+          >
+            <IoArrowForward />
+          </button>
+        </div>
+      )}
     </>
   );
 }
